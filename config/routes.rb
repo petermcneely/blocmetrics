38211f4e7781317f16df330d12751d62e23e1722
Peter McNeely <pete.mcneely@gmail.com>
2016-07-21 18:44:05 -0400

    Add API Controller for Events.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  namespace :api, defaults: {format: :json} do
+    match '/events', to: 'events#preflight', via: [:options]
+    resources :events, only: [:create]
+  end
+
   devise_for :users
 
   resources :registered_applications, except: [:update, :edit]
